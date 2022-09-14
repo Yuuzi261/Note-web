0a27a51f3a88e588ebdc7994022cdc8ed56f0e87
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2022-09-15 00:07:50 +0800

    refactor: Title element --> Headline element

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
     <BG />
-    <Title />
+    <Headline />
     <Category v-for="_ in Content" :title="_.title" :noteList="_.noteList" :imgsrc="_.imgsrc" :textTitle="_.textTitle" :text="_.text" :link="_.link" :key="_" />
 </template>
 
 <script>
     import BG from './components/index/BG.vue';
-    import Title from './components/index/Title.vue';
+    import Headline from './components/index/Headline.vue';
     import Category from './components/index/Category.vue';
 
     export default {
@@ -43,8 +43,8 @@
             }
         },
         components:{
-            BG, 
-            Title, 
+            BG,
+            Headline,
             Category
         }
     }
