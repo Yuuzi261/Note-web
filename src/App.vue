2b1c385430fac0f6fccab12808d186af0a696827
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2022-09-17 12:10:56 +0800

    refactor: transfer Content data to JSON

--- src/App.vue
+++ src/App.vue
@@ -8,38 +8,20 @@
     import BG from './components/index/BG.vue';
     import Headline from './components/index/Headline.vue';
     import Category from './components/index/Category.vue';
+    import NoteBlockData from '../static/NoteBlockData.json';
 
     export default {
         setup(){
-            const Content = [
-                {
-                    title: 'install',
-                    noteList: ['安裝 Python', '安裝 Visual Studio Code', '安裝 Git'],
-                    imgsrc: '../src/assets/index/1.png',
-                    textTitle: '安裝教學',
-                    text: '入門級簡易安裝教學',
-                    link: 'install'
-                },
-                {
-                    title: 'spice up',
-                    noteList: ['美化 Windows Terminal'],
-                    imgsrc: '../src/assets/index/2.png',
-                    textTitle: '個人化',
-                    text: '想來點不一樣的? 試試這個',
-                    link: 'spiceup'
-                },
-                {
-                    title: 'courses',
-                    noteList: ['MACHINE LEARNING 2022 SPRING'],
-                    imgsrc: '../src/assets/index/3.png',
-                    textTitle: '課程筆記',
-                    text: '存放我公開的上課筆記',
-                    link: 'courses'
-                }
-            ]
+            const Content = NoteBlockData.data
+            console.log(Content)
 
             return {
                 Content
+            }
+        },
+        data(){
+            return{
+                NoteBlockData
             }
         },
         components:{
