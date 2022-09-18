b6ed852915d19086216bf2902e293a7f43cd0b34
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2022-09-18 16:20:58 +0800

    test: path test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/Note-web/',
-	plugins: [vue()],
+	plugins: [vue()]
 	// build: {
 	// 	rollupOptions: {
 	// 		input: {
