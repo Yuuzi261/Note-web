fec775e67803f52266ab4682f6b5eb9d4a0f37ed
Yuuzi <lokasu0531@gmail.com>
2022-10-10 00:33:08 +0800

    refactor: App.vue: vue2 --> vue3

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,14 @@
     <Category v-for="_ in Content" :title="_.title" :noteList="_.noteList" :imgsrc="_.imgsrc" :imgalt="_.imgalt" :textTitle="_.textTitle" :text="_.text" :link="_.link" :key="_" />
 </template>
 
-<script>
+<script setup>
     import Welcome from './components/index/Welcome.vue';
     import BG from './components/index/BG.vue';
     import Headline from './components/index/Headline.vue';
     import Category from './components/index/Category.vue';
     import CategoryData from '../static/CategoryData.json';
 
-    export default {
-        setup(){
-            const Content = CategoryData.data
-
-            return {
-                Content
-            }
-        },
-        data(){
-            return{
-                CategoryData
-            }
-        },
-        components:{
-            BG,
-            Headline,
-            Category,
-            Welcome
-        }
-    }
-
-    
+    const Content = CategoryData.data;
 </script>
 
 <style scoped>
